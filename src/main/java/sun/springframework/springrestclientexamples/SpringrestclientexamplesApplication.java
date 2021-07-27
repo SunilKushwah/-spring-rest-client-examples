7d8fdf7f325477b5765942312f93acb2dea04fb4
Sunil Kushwah <kussunil513@gmail.com>
2021-07-27 10:47:13 +0400

    Initial commit.

---
+++ src/main/java/sun/springframework/springrestclientexamples/SpringrestclientexamplesApplication.java
@@ -0,0 +1,13 @@
+package sun.springframework.springrestclientexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestclientexamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringrestclientexamplesApplication.class, args);
+    }
+
+}
